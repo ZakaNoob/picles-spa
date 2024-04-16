14bac35b3bc4e0e3e348960147327f849d23e25f
zequiel <zequiel73@gmail.com>
2024-04-16 20:36:36 -0300

    feat:estruturando os arquivos

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Home } from './pages/Home/home.tsx'
+import { RouterProvider } from 'react-router-dom'
+import router  from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-    <Home />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
---
+++ src/pages/Pets/pets.tsx
@@ -0,0 +1,4 @@
+export function Pets() {
+    return <></>
+
+}
