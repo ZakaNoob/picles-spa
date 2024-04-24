a1c4d4db39cda780647ade2286d0ba0e904d496c
zequiel <zequiel73@gmail.com>
2024-04-23 21:35:27 -0300

    autorizacao da administracao do painel

---
+++ src/pages/Admin/PetList/PetList.tsx
@@ -0,0 +1,4 @@
+import { Panel } from "../../../components/layout/Panel"
+export function PetList() {
+    return <Panel>Listagem de pets do abrigo</Panel>
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Pets } from "./pages/Pets";
 import { PetDetails } from "./pages/PetDetails";
+import { Shelter } from "./pages/Admin/Shelter";
+import { PetList } from "./pages/Admin/PetList";
+import { AuthHoc } from "./components/common/AuthHoc/AuthHoc";
 
 const router = createBrowserRouter([{
     path: '/',
@@ -23,7 +26,24 @@ const router = createBrowserRouter([{
 },
 {
     path: '/admin',
-    element: <h1>Admin</h1>,
+    children: [
+        {
+          index: true,
+          element: <Shelter />
+        },
+        {
+            path: '/admin/pets',
+            element: <AuthHoc />,
+            children: [
+                {
+                    index: true,
+                    element: <PetList />,
+                },
+            ]
+        }
+        
+        
+    ],
 },
 ]) 
 
