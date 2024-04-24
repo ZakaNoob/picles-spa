a0d54059449dcc3443375095166a10b0a5f1751c
zequiel <zequiel73@gmail.com>
2024-04-23 22:19:49 -0300

    input com ref

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,5 +1,18 @@
+import styles from './Shelter.module.css'
+import { Button } from "../../../components/common/Button"
+import { Input } from "../../../components/common/Input"
 import { Panel } from "../../../components/layout/Panel"
 
 export function Shelter() {
-    return <Panel>Formulário de abrigo</Panel>
+    function submit (event) {
+     event.preventDeFault()
+     console.log('submit')
+    }
+    return <Panel>
+        <form className={styles.container}>
+            <Input label="nome" value='' />
+            <Button type="submit">Salvar dados</Button>
+        </form>
+
+    </Panel>
 }
